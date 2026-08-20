b449b82dea483f1b2888d6095d0a4c52ea26eb49
JTDR <johnwarrendelosreyes@gmail.com>
2026-08-20 09:51:34 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence
+} from "firebase/auth";
+import {
+  getFirestore
+} from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCHpiAbCeI6kt1WqCGlT9hT4i3hjuLt5Io",
   authDomain: "cvlpc-c9f07.firebaseapp.com",
@@ -13,5 +17,18 @@ const firebaseConfig = {
   appId: "1:700493126900:web:a98cb9ddcbccccb759d2a1"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+setPersistence(auth, browserLocalPersistence).catch((error) => {
+  console.error("Firebase persistence error:", error);
+});
+
+const db = getFirestore(app);
+
+export {
+  app,
+  auth,
+  db
+};
